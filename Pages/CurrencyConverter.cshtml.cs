2ca72d100577b95cc465d1d4f3910a188ff220bc
Tyronne282 <bradburntyronne@gmail.com>
2021-10-12 22:34:44 +0100

    Finished GBP to PHP conversion

--- Pages/CurrencyConverter.cshtml.cs
+++ Pages/CurrencyConverter.cshtml.cs
@@ -1,14 +1,30 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace CurrencyConverter.Pages
 {
     public class CurrencyConverterModel : PageModel
     {
+        public const Double GBP_PHP_RATE = 69.17;
+
+        [BindProperty]
+        public Double InputAmount { get; set; }
+        
+        [BindProperty]
+        public Double OutputAmount { get; set; }
+
+        private readonly ILogger<CurrencyConverterModel> _logger;
+
+        public CurrencyConverterModel(ILogger<CurrencyConverterModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
 
@@ -16,7 +32,21 @@ namespace CurrencyConverter.Pages
 
         public void OnPost()
         {
-
+            if (Double.IsNaN(InputAmount))
+            {
+                ViewData["Message"] = "Invalid type. " +
+                    "Please enter an amount.";
+            }
+            else if (Double.IsNegative(InputAmount))
+            {
+                ViewData["Message"] = "Invalid input. " +
+                    "Please enter a positive amount.";
+            }
+            else if (InputAmount > 0)
+            {
+                OutputAmount = InputAmount * GBP_PHP_RATE;
+                ViewData["Message"] = "Amount converted at ratio £1 : ₱69.17.";
+            }
         }
     }
 }
